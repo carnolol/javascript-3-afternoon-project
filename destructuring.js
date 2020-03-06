b4400d9e252aad58ab7f03f14dfb1150bc56a62a
Mike Chadwick <mikechadwick@Mikes-MacBook-Pro.local>
2020-03-06 15:17:35 -0700

    DONE

--- destructuring.js
+++ destructuring.js
@@ -118,11 +118,11 @@ function largeNumbers({first, second, third}){
 */
 
 function numberGroups({a, b, c}){
-  if (a > b.length && a > c.length) {
+  if (a.length > b.length && a.length > c.length) {
     return a
-  } else if ( b > a.length && b> c.length){
+  } else if ( b.length > a.length && b.length > c.length){
     return b
-  } else{
+  } else {
     return c
   }
 }
