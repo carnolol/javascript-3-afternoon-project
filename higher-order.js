07a6967b537b1bee8dc0f183a9ec42d1fd8d4ca0
Mike Chadwick <mikechadwick@Mikes-MacBook-Pro.local>
2020-03-06 17:49:53 -0700

    struggle is real

--- higher-order.js
+++ higher-order.js
@@ -20,8 +20,8 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-//Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+
+let evenNumbers = mixedNumbers.filter(e => e % 2 ===0)// = mixedNumbers.filter(/* Provide Your Callback Here */)
 
 
 
@@ -44,7 +44,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map((e) => {
+  return e * 1.07
+})
 
 
 
@@ -63,7 +65,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce((a,e) => {
+  return a + e 
+}, 0)
 
 
 
@@ -89,7 +93,11 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter((e) => {
+ if(e.CP > 200 ){
+   return monstersInYourPocket.monster
+ } // return monstersInYourPocket.CP
+}, 0)
 
 
 
